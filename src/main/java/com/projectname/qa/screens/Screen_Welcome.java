7f4ca3d7d16b3c2b04dfdff4d36f5058b895bc63
gaurav8758 <gaurav8758@gmail.com>
2017-10-25 09:03:23 -0500

    Adding code for Page object model

---
+++ src/main/java/com/projectname/qa/screens/Screen_Welcome.java
@@ -0,0 +1,32 @@
+package com.projectname.qa.screens;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.projectname.qa.util.ExtentTestManager;
+import com.projectname.qa.util.Verify;
+import com.relevantcodes.extentreports.LogStatus;
+
+
+public class Screen_Welcome {
+	WebDriver driver=null;
+	
+	//Object declaration
+	private By btnAgreeAndContinue = By.id("com.whatsapp:id/eula_accept");
+	private By TextWelcome = By.xpath("\\android.widget.TextView[contains(@text()='Welcome to WhatsApp')]");
+	
+	public Screen_Welcome(WebDriver tdriver) {
+		driver = tdriver;
+		ExtentTestManager.getTest().log(LogStatus.PASS, "<font color=Blue><B>Welcome Screen</B></font>");
+		// TODO Auto-generated constructor stub
+	}
+	
+	public WebElement btnAgreeAndContinue(){
+		return driver.findElement(btnAgreeAndContinue);
+	}
+	
+	public WebElement TextWelcome(){
+		return driver.findElement(TextWelcome);
+	}
+}
--- src/test/java/com/projectname/qa/testcases/Mobile_LoginTest.java
+++ src/test/java/com/projectname/qa/testcases/Mobile_LoginTest.java
@@ -6,12 +6,16 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.gargoylesoftware.htmlunit.javascript.host.Screen;
 import com.projectname.qa.base.MobileTestBase;
+import com.projectname.qa.screens.Screen_Welcome;
 import com.projectname.qa.util.ExtentTestManager;
+import com.projectname.qa.util.Verify;
 import com.relevantcodes.extentreports.LogStatus;
 
 public class Mobile_LoginTest extends MobileTestBase{
 	WebDriver driver;
+	Verify Assert;
 	//LoginPage loginPage;
 	//HomePage homePage;
 
@@ -21,6 +25,7 @@ public class Mobile_LoginTest extends MobileTestBase{
 	
 	public void setup(){
 		driver = getDriver();
+		Assert = new Verify(driver);
 		//loginPage = PageFactory.initElements(driver, LoginPage.class);
 		ExtentTestManager.getTest().log(LogStatus.PASS, "<font color=GREEN><B>Setup Completed</B></font>");
 	}
@@ -28,16 +33,23 @@ public class Mobile_LoginTest extends MobileTestBase{
 	@Test(priority=1, groups={"Testing","Mobile"})
 	public void Mobile_Login_Test001(){
 		setup();
-		driver.findElement(By.id("com.whatsapp:id/eula_accept")).click();
-		driver.findElement(By.id("com.whatsapp:id/cancel")).click();
-		driver.findElement(By.id("com.whatsapp:id/registration_phone")).sendKeys("2107739101");
+		
+		//Welcome Screen 
+		Screen_Welcome WelcomeScreen = PageFactory.initElements(driver, Screen_Welcome.class);
+		Assert.assertFalse(WelcomeScreen.TextWelcome().isDisplayed(), "'Welcome to Whatsapp' is not displayed in the screen.");
+		WelcomeScreen.btnAgreeAndContinue().click();
+		
+		//driver.findElement(By.id("com.whatsapp:id/eula_accept")).click();
+		//driver.findElement(By.id("com.whatsapp:id/cancel")).click();
+		
+		//driver.findElement(By.id("com.whatsapp:id/registration_phone")).sendKeys("2107739101");
 		
 		//Selecting a value
-		driver.findElement(By.id("com.whatsapp:id/registration_country")).click();
-		driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']")).click();
+		//driver.findElement(By.id("com.whatsapp:id/registration_country")).click();
+		//driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']")).click();
 	}
 	
-	@Test(priority=1, groups={"Testing","Mobile"},enabled=true)
+	@Test(priority=1, groups={"Testing","Mobile"},enabled=false)
 	public void Mobile_Login_Test002(){
 		setup();
 		driver.findElement(By.id("com.whatsapp:id/eula_accept")).click();
